c7843e1e5ada591564b56cdf82f6c0e8566ebad0
03Anushka <86141073+03Anushka@users.noreply.github.com>
2021-11-01 16:59:39 +0530

    2nd-text-detection

--- application.py
+++ application.py
@@ -1,3 +1,7 @@
+from flask_cors import CORS, cross_origin
+from flask import Flask, render_template, request, jsonify
+import requests
+import numpy as np
 import urllib
 from urllib.parse import urlencode
 import json
@@ -5,80 +9,80 @@ import validators
 
 url = 'https://neutrinoapi.net/bad-word-filter'
 params = {
-  'user-id': 'naren81',
-  'api-key': 'M4IbR5AwsH29MhiMGiJmDDyq0d01shupkPRRP0coQEG69vb9',
+    'user-id': 'naren81',
+    'api-key': 'M4IbR5AwsH29MhiMGiJmDDyq0d01shupkPRRP0coQEG69vb9',
     'content': 'https://en.wikipedia.org/wiki/Profanity'
 }
 
 
-
-import numpy as np
-# importing the necessary dependencies
-from flask import Flask, render_template, request,jsonify
-from flask_cors import CORS,cross_origin
-import joblib
 def _get_profane_prob(prob):
-  return prob[1]
-application = Flask(__name__) # initializing a flask app
-app=application
-@app.route('/',methods=['GET'])  # route to display the home page
+    return prob[1]
+
+
+application = Flask(__name__)  # initializing a flask app
+app = application
+
+
+@app.route('/', methods=['GET'])  # route to display the home page
 @cross_origin()
 def homePage():
     return render_template("index.html")
 
-@app.route('/predict',methods=['POST','GET']) # route to show the predictions in a web UI
+
+# route to show the predictions in a web UI
+@app.route('/predict', methods=['POST', 'GET'])
 @cross_origin()
 def index():
     if request.method == 'POST':
-        try:
-            te = []
-            #  reading the inputs given by the user
-            gre_score=(request.form['gre_score'])
-            te.append(gre_score)
-            is_research = request.form['research']
-            if(is_research=='TEXT'):
-                research=1
-                vectorizer = joblib.load('vectorizer.joblib')
-                model = joblib.load('model.joblib')
-                x = model.predict(vectorizer.transform(te))
-                y = np.apply_along_axis(_get_profane_prob, 1, model.predict_proba(vectorizer.transform(te)))
-                if x[0]==0:
-                    prediction = 'Sentence is not abusive'
-                    return render_template('results.html',prediction=prediction)
+        # try:
+        te = []
+        #  reading the inputs given by the user
+        gre_score = (request.form['gre_score'])
+        te.append(gre_score)
+        is_research = request.form['research']
+        if(is_research == 'TEXT'):
+
+            encoded_search = urllib.parse.quote(gre_score)
+
+            text_url = "https://neutrinoapi-bad-word-filter.p.rapidapi.com/bad-word-filter"
+
+            payload = f"content={encoded_search}&censor-character=*"
+            headers = {
+
+                'content-type': "application/x-www-form-urlencoded",
+                'x-rapidapi-host': "neutrinoapi-bad-word-filter.p.rapidapi.com",
+                'x-rapidapi-key': "171078cc74mshcece1182e710d91p1508f8jsn185172894b07"
+            }
+
+            response = requests.request(
+                "POST", text_url, data=payload, headers=headers)
+
+            print(response.text)
+            return render_template('results.html', prediction=response.text)
+
+        if(is_research == 'URL'):
+            if validators.url(gre_score) == True:
+                gre_score = (request.form['gre_score'])
+                params['content'] = gre_score
+                encoded_params = urlencode(params).encode('utf8')
+                response = urllib.request.urlopen(url, data=encoded_params)
+                result = json.loads(response.read())
+                if result['is-bad'] == True:
+                    t = result['bad-words-list']
+                    st = ",".join(t)
+                    prediction = "Url is abusive and it contains following abusive words " + st
+                    return render_template('results.html', prediction=prediction)
                 else:
-                    temp = round(100*y[0])
-                    prediction = 'Sentence is abusive with score '+ str(temp) +" percent"
-                    print('prediction is', prediction)
-            # showing the prediction results in a UI
-                    return render_template('results.html',prediction=prediction)
-            if(is_research=='URL'):
-                if validators.url(gre_score)==True:
-                    gre_score=(request.form['gre_score'])
-                    params['content']=gre_score
-                    encoded_params = urlencode(params).encode('utf8')
-                    response = urllib.request.urlopen(url, data = encoded_params)
-                    result = json.loads(response.read())
-                    if result['is-bad']==True:
-                        t = result['bad-words-list']
-                        st = ",".join(t)
-                        prediction = "Url is abusive and it contains following abusive words "+ st
-                        return render_template('results.html',prediction=prediction)
-                    else:
-                        prediction = 'Url is not abusive'
-                        return render_template('results.html',prediction=prediction)
-                else:
-                    prediction = "Not Valid Url"
-                    return render_template('results.html',prediction=prediction)
-                    
-        except Exception as e:
-            print('The Exception message is: ',e)
-            return 'something is wrong'
-    # return render_template('results.html')
+                    prediction = 'Url is not abusive'
+                    return render_template('results.html', prediction=prediction)
+            else:
+                prediction = "Not Valid Url"
+                return render_template('results.html', prediction=prediction)
+
     else:
         return render_template('index.html')
 
 
-
 if __name__ == "__main__":
     #app.run(host='127.0.0.1', port=8001, debug=True)
-	app.run(debug=True) # running the app
+    app.run(debug=True)  # running the app
